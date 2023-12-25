6ec640f5ec1aee393e2de5fc8e4d7b57f9a8bd89
채상희 <chaesanghui142@gmail.com>
2023-12-25 23:29:46 +0900

    12/25 - Thread

---
+++ src/chap_12/_02_Runnable.java
@@ -0,0 +1,29 @@
+package chap_12;
+
+import chap_12.clean.CleanRunnable;
+
+public class _02_Runnable {
+    public static void main(String[] args) {
+        CleanRunnable cleanRunnable = new CleanRunnable();
+        Thread thread = new Thread(cleanRunnable);
+        thread.start();
+
+        cleanByBoss();
+    }
+
+    public static void cleanBySelf() {
+        System.out.println("-- 혼자 청소 시작 --");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("(혼자) " + i + "번방 청소 중");
+        }
+        System.out.println("-- 혼자 청소 끝 --");
+    }
+
+    public static void cleanByBoss() {
+        System.out.println("-- 사장 청소 시작 --");
+        for (int i = 1; i <= 10; i+=2) {
+            System.out.println("(사장) " + i + "번방 청소 중");
+        }
+        System.out.println("-- 사장 청소 끝 (Thread) --");
+    }
+}
