5a3813daf81d1d9c8d40f8c56b28d39ce8d31d9b
lettuce <chaesanghui142@gmail.com>
2023-12-26 10:56:08 +0900

    12/26 - Thread 문풀

--- src/chap_12/_Quiz_12.java
+++ src/chap_12/_Quiz_12.java
@@ -2,28 +2,67 @@ package chap_12;
 
 public class _Quiz_12 {
     public static void main(String[] args) {
+        readyItem readyItem = new readyItem();
 
-        Runnable itemA = new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 5; i++) {
-                }
-            }
+        Runnable readyA = () -> {
+            readyItem.ready("A");
         };
-
-        Runnable itemB = new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 5; i++) {
-                }
-            }
+        Runnable readyB = () -> {
+            readyItem.ready("B");
         };
-
-        Thread threadA = new Thread(itemA);
-        Thread threadB = new Thread(itemB);
-        threadA.start();
-        threadB.start();
+        Thread threadSet = getThread(readyA, readyB);
+        threadSet.start();
     }
 
+    private static Thread getThread(Runnable readyA, Runnable readyB) {
+        Thread threadA = new Thread(readyA);
+        Thread threadB = new Thread(readyB);
+        threadA.start();
+        threadB.start();
+        try {
+            threadA.join();
+            threadB.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
+        Runnable runnableSet = new Runnable() {
+            @Override
+            public void run() {
+                readySet readySet = new readySet();
+                readySet.ready();
+            }
+        };
+        return new Thread(runnableSet);
+    }
 }
+
+class readyItem {
+    int step = 1;
+    void ready(String name) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(name + " 상품 준비 " + step + "/5");
+            if (step >= 5) {
+                step = 0;
+                System.out.println("-- " + name +  "상품 준비 완료 --");
+            }
+            step++;
+        }
+    }
+}
+
+class readySet {
+    int step = 1;
+    void ready() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("세트 상품 포장 " + step + "/5");
+            if (step >= 5) {
+                step = 0;
+                System.out.println("== 세트 상품 포장 완료 ==");
+            }
+            step++;
+        }
+    }
+}
+
+
